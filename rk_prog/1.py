59095a5501b27c3ffef552d3b2cbdefb12bac3a3
Nikita <42384037+Nikdevelop@users.noreply.github.com>
2023-11-06 19:23:02 +0300

    Update 1.py

--- rk_prog/1.py
+++ rk_prog/1.py
@@ -1,3 +1,14 @@
+def rotate_matrix(matrix):
+    n = len(matrix)
+    for i in range(0, n, 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - 1 - j][i]
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+            matrix[j][n - 1 - i] = temp
+    return matrix
+
 el_cnt_1 = int(input("Введите количество элементов в первом списке: "))
 lst_1 = list(map(int, input().split()))
 el_cnt_2 = int(input("Введите количество элементов во втором списке: "))
