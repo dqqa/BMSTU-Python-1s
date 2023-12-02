3564781f49826fa339c3a1c7f5c23f82911d2387
Nikita <42384037+Nikdevelop@users.noreply.github.com>
2023-12-02 18:06:13 +0300

    12th lab done

--- 12_lab_text.py
+++ 12_lab_text.py
@@ -1,6 +1,20 @@
 # Выполнил Жижин Никита. Группа ИУ7-11Б
 # Лабораторная работа №12 “Текстовый процессор”
 import utils
+import re
+
+
+TEXT = (
+    "Все счастливые семьи похожи друг на друга, каждая несчастливая семья несчастлива по-своему. "
+    "Все смешалось в доме Облонских. Жена узнала, что муж был в связи с бывшею в их доме гувернанткой, "
+    "и объявила мужу, что не может жить с ним в одном доме. Положение это продолжалось уже третий день и му"
+    "чительно чувствовалось и самими супругами, и всеми членами семьи, и домочадцами. Все члены семьи и"
+    " домочадцы 1/2, что нет 81+9 смысла в их сожительстве и что на каждом постоялом дворе случай"
+    "но сошедшиеся люди более связаны между собой, чем они, члены семьи и домочадцы Облонских. Жена "
+    "не выходила из своих комнат, мужа третий день не было дома. Дети бегали по всему дому, как потер"
+    "янные; англичанка поссорилась с экономкой и написала записку приятельнице, прося приискать ей но"
+    "вое место; повар ушел вчера со двора, 12/3 во время с5-1амого обеда; черная кухарка и кучер просили расчета."
+)
 
 
 def ask_menu() -> int:
@@ -19,63 +33,109 @@ def ask_menu() -> int:
     return utils.safe_menu_input(1, 8)
 
 
-def separate_by_lines(text) -> list[str]:
-    pass
+def separate_by_lines(text: str) -> list[str]:
+    lines = []
+    splitted = text.split(" ")
+    line_length = len(text) // 10
+    current_line = ""
+    for word in splitted:
+        if len(current_line + word) > line_length:
+            lines.append(current_line.strip())
+            current_line = ""
+        current_line += " " + word
+    lines.append(current_line.strip())
+
+    return lines
 
 
-def left_align(text):
-    pass
+def left_align(text: list[str]):
+    max_size = len(max(text, key=lambda x: len(x.strip())))
+    for i in range(len(text)):
+        text[i] = f"{text[i].strip():<{max_size}}"
 
 
-def right_align(text):
-    pass
+def right_align(text: list[str]):
+    max_size = len(max(text, key=lambda x: len(x.strip())))
+    for i in range(len(text)):
+        text[i] = f"{text[i].strip():>{max_size}}"
 
 
-def width_align(text):
-    pass
+def width_align(text: list[str]):
+    max_size = len(max(text, key=lambda x: len(x.strip())))
+    for i in range(len(text)):
+        text[i] = text[i].strip()
+        gap_width, max_replace = divmod(
+            max_size - len(text[i]) + text[i].count(" "), text[i].count(" ")
+        )
+        text[i] = (
+            text[i]
+            .replace(" ", " " * gap_width)
+            .replace(" " * gap_width, " " * (gap_width + 1), max_replace)
+        )
 
 
-def delete_word(text):
-    pass
+def replace_word(text: list[str], word: str, word_to_replace: str):
+    for i in range(len(text)):
+        if word_to_replace:
+            text[i] = re.sub(f"{word}", word_to_replace, text[i])
+        else:
+            text[i] = re.sub(f"\\s*{word}\\s*", word_to_replace, text[i])
 
 
-def replace_word(text):
-    pass
+def solve_math(text: list[str]):
+    regexp = r"(\d+)([\+\-\*\/])(\d+)"
+    for i in range(len(text)):
+        string = text[i]
+        for obj in re.finditer(regexp, text[i]):
+            a, op, b = obj.groups()
+            a=int(a)
+            b=int(b)
+            result = 0
+            match op:
+                case "+":
+                    result = a + b
+                case "-":
+                    result = a - b
+                case "/":
+                    result = a // b
+                case "*":
+                    result = a * b
+            string = re.sub(f"{a}\{op}{b}", str(result), string)
+        text[i] = string
 
 
-def solve_math(text):
-    pass
-
-
-def delete_shortest(text):
+def delete_shortest(text: list[str]):
     pass
 
 
 def main():
-    text = input(">>> Введите текст: ")
-    lines = separate_by_lines(text)
+    lines = separate_by_lines(TEXT)
     while True:
         usr_input = ask_menu()
         match usr_input:
             case 1:
-                pass
+                left_align(lines)
             case 2:
-                pass
+                right_align(lines)
             case 3:
-                pass
+                width_align(lines)
             case 4:
-                pass
+                word = input(">>> Введите слово, которое необходимо удалить: ")
+                replace_word(lines, word, "")
             case 5:
-                pass
+                word = input(">>> Введите слово, которое необходимо удалить: ")
+                new_word = input(f">>> Введите слово, на которое необходимо заменить {word}: ")
+                replace_word(lines, word, new_word)
             case 6:
-                pass
+                solve_math(lines)
             case 7:
-                pass
+                delete_shortest(lines)
             case 8:
                 exit(0)
 
-        utils.press_anykey()
-        utils.clear_terminal()
+        print(*lines, sep="\n")
+        # utils.press_anykey()
+        # utils.clear_terminal()
 
 
 if __name__ == "__main__":
